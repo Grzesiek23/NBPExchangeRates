1f8e89630f967adde59e3c6aadf9ae2ce7e57de6
Grzesiek <g.pasko@gmail.com>
2023-08-26 12:58:03 +0200

    Init commit

---
+++ src/NBPExchangeRates.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NBPExchangeRates.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
