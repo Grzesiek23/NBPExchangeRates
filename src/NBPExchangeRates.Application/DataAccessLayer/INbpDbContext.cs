7451d10330fc5baffd03989a717f41ce59553902
Grzegorz Paśko <grzegorz.pasko@smeo.pl>
2023-08-30 17:00:32 +0200

    Added SaveChangesAsync for INbpDbContext

--- src/NBPExchangeRates.Application/DataAccessLayer/INbpDbContext.cs
+++ src/NBPExchangeRates.Application/DataAccessLayer/INbpDbContext.cs
@@ -8,4 +8,5 @@ public interface INbpDbContext
     public DbSet<Currency> Currencies { get; set; }
     public DbSet<ExchangeRate> ExchangeRates { get; set; }
     DbSet<ExchangeRateSnapshot> ExchangeRangeSnapshots { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
