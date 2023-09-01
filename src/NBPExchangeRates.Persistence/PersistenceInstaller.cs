0eb5e6ca04db96eada06dbed9c5d7b99b9d1fa9f
Grzesiek <g.pasko@gmail.com>
2023-09-01 12:48:53 +0200

    Implemented dynamic database connection for different environments

--- src/NBPExchangeRates.Persistence/PersistenceInstaller.cs
+++ src/NBPExchangeRates.Persistence/PersistenceInstaller.cs
@@ -10,9 +10,16 @@ public static class PersistenceInstaller
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<NbpDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("NpbConnection")));
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        var connectionString = configuration.GetConnectionString("NbpConnection");
+        
+        if (environment == "PRODUCTION") connectionString = Environment.GetEnvironmentVariable("NBP_CONNECTION_STRING");
 
+        if (string.IsNullOrWhiteSpace(connectionString))
+            throw new ArgumentNullException(connectionString);
+        
+        services.AddDbContext<NbpDbContext>(options => options.UseSqlServer(connectionString));
+        
         services.AddScoped<INbpDbContext, NbpDbContext>();
 
         return services;
